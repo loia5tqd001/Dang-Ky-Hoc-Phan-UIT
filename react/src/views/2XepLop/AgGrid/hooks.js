c1946bb4c4fdb83167473fc3f9e526068c9007fb
Nguyễn Huỳnh Lợi <loia5tqd001@gmail.com>
2021-08-03 10:31:22 +0700

    Upgrade packages

--- react/src/views/2XepLop/AgGrid/hooks.js
+++ react/src/views/2XepLop/AgGrid/hooks.js
@@ -5,7 +5,7 @@ import { useDebouncedCallback } from 'use-debounce';
 export const useDebouncedStoreColumnState = (time = 500) => {
   const dispatch = useDispatch();
 
-  const [debouncedStoreColumnState] = useDebouncedCallback(({ columnApi }) => {
+  const debouncedStoreColumnState = useDebouncedCallback(({ columnApi }) => {
     dispatch(setAgGridColumnState(columnApi.getColumnState()));
   }, time);
 
