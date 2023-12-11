456347816caba67215ea6ff50d772a0f32ce9d03
Nguyễn Huỳnh Lợi <loia5tqd001@gmail.com>
2023-12-11 18:18:44 +0800

    fix

--- react/src/views/components/NeedStep1.tsx
+++ react/src/views/components/NeedStep1.tsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { ROUTES } from '../../constants';
 
 function NeedStep1Warning() {
+  const location = useLocation();
+
   return (
     <div style={{ textAlign: 'center' }}>
       <h2>Có vẻ như bạn chưa chọn file excel ở bước 1</h2>
-      <Link to={ROUTES._1ChonFileExcel.path}>Về bước 1</Link>
+      <Link to={ROUTES._1ChonFileExcel.path + location.search}>Về bước 1</Link>
     </div>
   );
 }
