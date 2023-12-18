32d4e6f982c6ca760fbae6c558e8a7b3dbfd4189
Nguyễn Huỳnh Lợi <loia5tqd001@gmail.com>
2023-12-18 08:49:54 +0800

    chore: add new tracking

--- react/src/views/1ChonFileExcel/index.tsx
+++ react/src/views/1ChonFileExcel/index.tsx
@@ -1,5 +1,6 @@
 import { Alert } from '@mui/material';
 import { selectDataExcel, useTkbStore } from '../../zus';
+import { trackEvent } from '../../tracking';
 import SelectExcelButton from './SelectExcelButton';
 
 function Index() {
@@ -19,6 +20,11 @@ function Index() {
           href="https://github.com/loia5tqd001/Dang-Ky-Hoc-Phan-UIT?tab=readme-ov-file#c%C3%A1c-update-%E1%BB%9F-phi%C3%AAn-b%E1%BA%A3n-2024"
           target="_blank"
           rel="noreferrer"
+          onClick={() => {
+            trackEvent.page1({
+              action: '2024_update_link_click',
+            });
+          }}
         >
           đây
         </a>
@@ -30,7 +36,16 @@ function Index() {
         variant="outlined"
       >
         Video hướng dẫn sử dụng script đăng ký nhanh:{' '}
-        <a href="https://www.youtube.com/watch?v=DsLUHgX_xzs" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.youtube.com/watch?v=DsLUHgX_xzs"
+          target="_blank"
+          rel="noreferrer"
+          onClick={() => {
+            trackEvent.page1({
+              action: 'script_instruction_video_link_click',
+            });
+          }}
+        >
           Xem ngay
         </a>
       </Alert>
