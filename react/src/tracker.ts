6dd1e36b57a529e1e40824fc58bca50b6be41a37
Nguyễn Huỳnh Lợi <loia5tqd001@gmail.com>
2023-12-21 09:43:49 +0800

    feat: try optimizing tracking bandwidth

--- react/src/tracker.ts
+++ react/src/tracker.ts
@@ -1,27 +1,26 @@
 import { logEvent } from 'firebase/analytics';
 import { Timestamp, doc, setDoc } from 'firebase/firestore';
-import { throttle } from 'lodash';
 import ReactGA from 'react-ga4';
 import { getBrowserName, getOsName, getVisitorFingerprint } from './tracking.utils';
 import { log } from './utils';
 import { analytics, db, isProd } from '.';
 
 type AllowedPropertyValues = string | number | boolean | null | undefined;
-type EventGroup = 'tkb_table' | 'so_tc' | 'drawer' | 'page1' | 'page2' | 'page3';
+type EventGroup = 'tkb_table' | 'so_tc' | 'drawer' | 'page1' | 'page2' | 'page3' | 'page';
 type EventAction = 'clicked' | 'hovered' | 'toggled' | 'changed' | 'shown' | 'resulted';
-type EventRecord = {
+type EventRecord<IsFirebase extends boolean = false> = {
   name: `[${EventGroup}] ${string}${EventAction}` | 'page_loaded';
   location: string;
-  time: Timestamp;
+  time: IsFirebase extends true ? Timestamp : number;
   data: {
     [propertyName: string]: AllowedPropertyValues;
   };
 };
-type SessionRecord = {
+type SessionRecord<IsFirebase extends boolean = false> = {
   visitorId: string;
   startLocation: string;
-  startTime: Timestamp;
-  endTime: Timestamp;
+  startTime: IsFirebase extends true ? Timestamp : number;
+  endTime: IsFirebase extends true ? Timestamp : number;
   engagementTime: number;
   timeZone: string;
   browser: string;
@@ -29,9 +28,14 @@ type SessionRecord = {
   hasAdBlocker: boolean;
   leftDrawerInitiallyOpen: boolean;
   referral: string;
-  events: EventRecord[];
+  events: EventRecord<IsFirebase>[];
 };
+const LOCAL_STORAGE_KEY = 'TRACKING_EVENTS';
 
+/**
+ * cache events to localStorage
+ * dump events to firestore when visibility changed to hidden, or in a new session if previous session was closed unexpectedly
+ */
 export const buildTracker = () => {
   const visitorId = getVisitorFingerprint();
   const startLocation = window.location.href;
@@ -42,7 +46,8 @@ export const buildTracker = () => {
   const startTime = Date.now();
   let leftDrawerInitiallyOpen: boolean;
   let hasAdBlocker: boolean;
-
+  const sessionId = `${visitorId}-${startTime}`;
+  type TrackingEventsLocalStorage = Record<typeof sessionId, SessionRecord>;
   const events: EventRecord[] = [];
 
   const track = (eventName: EventRecord['name'], properties?: EventRecord['data']) => {
@@ -51,10 +56,10 @@ export const buildTracker = () => {
     events.push({
       name: eventName,
       location: window.location.href,
-      time: Timestamp.now(),
+      time: Date.now(),
       data: properties ?? {},
     });
-    dump();
+    cacheToLocalStorage();
 
     // firebase analytics
     logEvent(analytics, eventName, properties);
@@ -76,40 +81,83 @@ export const buildTracker = () => {
     }
   };
 
-  const dump = throttle(
-    () => {
-      const finalStartTime = Timestamp.fromMillis(startTime);
-      const finalEndTime = Timestamp.now();
-      const sessionRecord: SessionRecord = {
-        visitorId,
-        startLocation,
-        startTime: finalStartTime,
-        endTime: finalEndTime,
-        engagementTime: finalEndTime.seconds - finalStartTime.seconds,
-        timeZone,
-        browser,
-        os,
-        hasAdBlocker,
-        leftDrawerInitiallyOpen,
-        referral,
-        events,
-      };
-      log('>>dump', sessionRecord);
-      if (isProd) {
-        const newOrExistingDoc = doc(db, 'trackingEvents', `${visitorId}-${startTime}`);
-        setDoc(newOrExistingDoc, sessionRecord);
-      }
-    },
-    3000,
-    { leading: false, trailing: true },
-  );
+  const getLocalSessionRecord = (): SessionRecord<false> => {
+    const endTime = Date.now();
+    return {
+      visitorId,
+      startLocation,
+      startTime,
+      endTime,
+      engagementTime: Math.floor(endTime / 1000 - startTime / 1000), // as seconds
+      timeZone,
+      browser,
+      os,
+      hasAdBlocker,
+      leftDrawerInitiallyOpen,
+      referral,
+      events,
+    };
+  };
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/sendBeacon#sending_analytics_at_the_end_of_a_session
-  // document.addEventListener('visibilitychange', () => {
-  //   if (document.visibilityState === 'hidden') {
-  //     dump();
-  //   }
-  // });
+  const getFirebaseSessionRecord = (localSessionRecord: SessionRecord<false>): SessionRecord<true> => {
+    return {
+      ...localSessionRecord,
+      startTime: Timestamp.fromMillis(localSessionRecord.startTime),
+      endTime: Timestamp.fromMillis(localSessionRecord.endTime),
+      events: localSessionRecord.events.map((event) => ({
+        ...event,
+        time: Timestamp.fromMillis(event.time),
+      })),
+    };
+  };
+
+  const getCachedTrackingFromLocalStorage = (): TrackingEventsLocalStorage => {
+    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+  };
+
+  const cacheToLocalStorage = () => {
+    if (events.length) {
+      const existingEvents = getCachedTrackingFromLocalStorage();
+      const sessionRecord = getLocalSessionRecord();
+      log('>>cacheToLocalStorage', { existingEvents, sessionId, sessionRecord });
+      existingEvents[sessionId] = sessionRecord;
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(existingEvents));
+    }
+  };
+
+  const dumpToFirestore = async () => {
+    // has taken care of:
+    // - multiple tabs (multiple sessions) opened simultaneously
+    // - events from previous sessions (closed unexpectedly), but not yet dumped to firestore
+    const existingEvents = getCachedTrackingFromLocalStorage();
+    log('>>dumpToFirestore', { existingEvents });
+    const promises = Object.entries(existingEvents).map(([sessionId, sessionRecord]) => {
+      const finalSessionId = isProd ? sessionId : `__DEV__${sessionId}`;
+      const newOrExistingDoc = doc(db, 'trackingEvents', finalSessionId);
+      return setDoc(newOrExistingDoc, getFirebaseSessionRecord(sessionRecord)).then(() => {
+        delete existingEvents[sessionId];
+      });
+    });
+    await Promise.all(promises);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(existingEvents));
+  };
+
+  document.addEventListener('visibilitychange', async (e) => {
+    log('>>visibilitychange', document.visibilityState);
+    // add this to more accurately track engagement time
+    // since dumping to localStorage is a sync operation, it'll be sured operated, whereas dumping to firestore is async and not sure,
+    // in that case, we leveraged localStorage to leave a dirt for future sessions to dump to firestore in case the current session was closed unexpectedly
+    track('[page] visibility_changed', { visibility: document.visibilityState });
+    if (document.visibilityState === 'hidden') {
+      dumpToFirestore();
+    }
+  });
+
+  const existingEvents = getCachedTrackingFromLocalStorage();
+  // if there are trackings from previous sessions, dump them to firestore
+  if (Object.keys(existingEvents).length) {
+    dumpToFirestore();
+  }
 
   return {
     track,
